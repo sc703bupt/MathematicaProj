2f1c0a465c7232607aafd73f60adad716e908242
shenchen <sc1_1@163.com>
2015-02-26 00:23:10 +0800

    impl more methods in FormulaCalculater.java

--- src/com/formulacalculate/FormulaCalculater.java
+++ src/com/formulacalculate/FormulaCalculater.java
@@ -2,9 +2,9 @@ package com.formulacalculate;
 
 import java.math.BigInteger;
 import java.util.Scanner;
-import com.wolfram.jlink.*;
 import java.util.List;
 import java.util.ArrayList;
+import com.wolfram.jlink.*;
 
 public class FormulaCalculater {
 	private KernelLink kernelLink;
@@ -21,16 +21,54 @@ public class FormulaCalculater {
 		}
 	}
 	
-	List<BigInteger> calculateToList(String expr) {
-		//TODO(shenchen):transfer string to list
-		return new ArrayList<BigInteger>();
-	}
-	
-	String calculateToString(String expr) throws Exception{
+	// expr is the whole expr from webpage MATHEMATICA option
+	// we should split it and campare with sample, leave sample 
+	// null means don't compare
+	public String calculateToString(String expr, String sample) throws Exception{
 		if (kernelLink == null) {
 			throw new Exception("KernelLink");
 		}
-		return kernelLink.evaluateToOutputForm(expr, 0);
+		// handle expr
+		List<String> exprList = splitExpr(expr);
+		List<String> retList = new ArrayList<String>();
+		for (String e : exprList) {
+			retList.add(kernelLink.evaluateToOutputForm(e, 0));
+		}
+		
+		if (sample == null) {//don't compare and return first elem not null
+			for (String ret : retList) {
+				if (ret != null) return ret;
+			}
+		} else {
+			//TODO(shenchen):impl compare logic
+		}
+		return null;
+	}
+	
+	// according to different rules, split expr and try
+	List<String> splitExpr(String expr) {
+		//TODO(shenchen):impl different rules, now we just don't split
+		List<String> ret = new ArrayList<String>();
+		ret.add(expr);
+		return ret;
+	}
+	
+	public List<BigInteger> calculateToList(String expr, String sample) throws Exception {
+		String retStr = calculateToString(expr, sample);
+		// convert string to list
+		if (retStr == null || retStr.isEmpty()) {
+			return null;
+		}
+		String numberStr = retStr.substring(1, retStr.length()-1);
+		System.out.println(numberStr);
+		String[] numberStrList = numberStr.split(", ");
+		List<BigInteger> numberList = new ArrayList<BigInteger>();
+		for (String numStr : numberStrList) {
+			System.out.println(numStr);
+			BigInteger num = new BigInteger(numStr);
+			numberList.add(num);
+		}
+		return numberList;
 	}
 	
 	// remember call this method when never use KernelLink
@@ -46,7 +84,8 @@ public class FormulaCalculater {
 				System.out.println("Please enter expr:");
 				Scanner s = new Scanner(System.in);
 				String expr = s.nextLine();
-				String A = fc.calculateToString(expr);
+				String A = fc.calculateToString(expr, null);
+				//List<BigInteger> B = fc.calculateToList(expr);
 				System.out.println("Response: " + A);
 			}
 		} catch (MathLinkException e) {
