1779aa595a0cdd1fc23237922d4be44aafe4b4b6
shenchen <sc1_1@163.com>
2015-03-08 10:54:54 +0800

    This commit contains: 1.add ignore rules in .gitignore for xxxTester.java 2.add Constant.java for all constans 3.impl some part of ShortestUniquePrefixFinder.java

--- src/com/shortesetuniqueprefix/ShortestUniquePrefixFinder.java
+++ src/com/shortesetuniqueprefix/ShortestUniquePrefixFinder.java
@@ -1,9 +1,64 @@
 package com.shortesetuniqueprefix;
 
-public class ShortestUniquePrefixFinder {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import com.util.*;
 
+public class ShortestUniquePrefixFinder {
+	private String absOutputFolderName;
+	
+	public ShortestUniquePrefixFinder(String absOutputFolderName) {
+		this.absOutputFolderName = absOutputFolderName;
+	}
+	
+	/* Step 1: combine two source files and divide data into several folders/files
+	   Step 2: calculate shortest unique prefix recursively
+	*/
+	public int find(String absSampleFileName, String absExprFileName, boolean isOutputOverride) throws IOException{
+		if (absOutputFolderName == null || absOutputFolderName.isEmpty()) {
+			throw new IOException("absOutputFolderName is null or empty.");
+		}
+		
+		File absOutputFolder = new File(absOutputFolderName);
+		
+		if (absOutputFolder.exists()) {
+			if (isOutputOverride) {
+				//TODO
+			}
+		}
+		if (!absOutputFolder.isDirectory()) {
+			throw new IOException("absOutputFolderName is not folder.");
+		}
+		
+		if (absSampleFileName == null || absExprFileName == null ||
+			absSampleFileName.isEmpty() || absExprFileName.isEmpty()) {
+			throw new IOException("sampleFileName or exprFileName is null or empty.");
+		}
+		
+		File sampleFile = new File(absSampleFileName);
+		File exprFile = new File(absExprFileName);
+		
+		BufferedReader sampleFileReader = new BufferedReader(new FileReader(sampleFile));
+		BufferedReader exprFileReader = new BufferedReader(new FileReader(exprFile));
+		// combine and divide
+		Map.Entry<File, Integer> result = combineAndDivide(sampleFileReader, exprFileReader, 3);
+		return result.getValue();
+	}
+	
+	
+	private Map.Entry<File, Integer> combineAndDivide(BufferedReader sampleFileReader, 
+			BufferedReader exprFileReader, int levelForDivide) {
+		for (int i = 1; i <= Constant.TOTAL_PAGES_COUNT; i++) {	
+			//String
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		ShortestUniquePrefixFinder supf = new ShortestUniquePrefixFinder("c:\\rootFolder");
+		System.out.println(supf.find("", "", true));
+	}
 }
--- src/com/util/Util.java
+++ src/com/util/Util.java
@@ -1,10 +1,10 @@
 package com.util;
-import java.io.BufferedReader;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
-
 
 public class Util {
 	public static String getFileNameFromID(int id, int idLength) {
@@ -43,5 +43,46 @@ public class Util {
 	public static List<String> readContentByRange(FileReader fr, String key, int startId, int endId) {
 		//TODO(shenchen):impl
 		return null;
+	}
+	
+	// delete all sub-files and sub-folders, then the folder itself
+    public static void delFolder(String absFolderName) {
+    	try {
+    		delAllFile(absFolderName);
+    		File myFilePath = new File(absFolderName);
+    		myFilePath.delete();
+    	} catch (Exception e) {
+    		e.printStackTrace(); 
+    	}
+    }
+
+    // called by delFolder
+	private static void delAllFile(String absFolderName) throws IOException {
+	    File file = new File(absFolderName);
+	    if (!file.exists() || !file.isDirectory()) {
+	    	throw new IOException("absFolderName is not exist or not a directory name.");
+	    }
+	    String[] fileList = file.list();
+	    File absFile = null;
+	    for (int i = 0; i <= fileList.length-1; i++) {
+	    	// generate absFile
+	    	if (absFolderName.endsWith("\\")) {
+	    		absFile = new File(absFolderName + fileList[i]);
+	        } else {
+	        	absFile = new File(absFolderName + "\\" + fileList[i]);
+	        }
+	    	
+	    	// if single file, just delete
+	        if (absFile.isFile()) {
+	        	absFile.delete();
+	        	continue;
+	        }
+	        
+	        // if directory, recursively delete
+	        if (absFile.isDirectory()) {
+	        	delAllFile(absFolderName + "\\" + fileList[i]);
+	            delFolder(absFolderName + "\\" + fileList[i]);
+	        }
+	    }
 	}
 }
